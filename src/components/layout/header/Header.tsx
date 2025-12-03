e62cc82a05a4bafa4abf11905db0e1f54c809845
Rafael Mendonca <rafael.decarvalho@castgroup.com.br>
2025-12-03 11:58:38 -0300

    ajustes header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,7 +1,18 @@
+import "./index.css"
+
 export function Header() {
   return (
-    <header>
-      <h1>Welcome to BrainHub</h1>
-    </header>
+    <div className="header" id="header">
+      <div className="logo_header">
+        <img src="criatividade.png" alt="BrainHub Logo" />
+      </div>
+
+      <div className="navigation_header">
+        <a href="#">Home</a>
+        <a href="#">Artigos</a>
+        <a href="#">Sobre Nós</a>
+        <a href="#">Contato</a>
+      </div>
+    </div>
   )
 }
